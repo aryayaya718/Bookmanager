60fd1f41364ae59b891fce95e1cb9640fe9c69ed
aryayaya718 <jainarya846@gmail.com>
2026-03-22 14:01:53 +0530

    initial commit

---
+++ src/test/java/com/arya/bookmanagerjpa/BookmanagerjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arya.bookmanagerjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagerjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
